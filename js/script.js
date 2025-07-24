75957fa2b7fff6bee1120b1519e0fae67156d0d5
NicolasMZF <nicolasmartinezf137@gmail.com>
2025-07-24 02:36:15 -0500

    Portfolio upgraded new version

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
-const svg = document.getElementById('starrySky');
-        function createStars(numStars){
-          for (let i = 0; i < numStars; i++) {
-            const star = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-            const x = Math.random() * 100;
-            const y = Math.random() * 100;
-            const radius = Math.random() * 2 + 0.5;
+function createStars(numStars){
+  const svg = document.getElementById('starrySky');
+  for (let i = 0; i < numStars; i++) {
+    const star = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    const x = Math.random() * 100;
+    const y = Math.random() * 100;
+    const radius = Math.random() * 2 + 0.5;
 
-            star.setAttribute('cx', `${x}%`);
-          star.setAttribute('cy', `${y}%`);
-          star.setAttribute('r', radius);
-          star.setAttribute('fill', 'white');
+  star.setAttribute('cx', `${x}%`);
+  star.setAttribute('cy', `${y}%`);
+  star.setAttribute('r', radius);
+  star.setAttribute('fill', 'white');
 
-          svg.appendChild(star);
-          }
-        }
+  svg.appendChild(star);
+  }
+}
 
-        createStars(100);
+function scrollToCenter(event, target) {
+  event.preventDefault();
+  const element = document.getElementById(target);
+  const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+  var offset = 0;
+  if(target=='formation'){
+    var offset = 200;
+  } else {
+    var offset = 100; //adjust the value for better position future me
+  }
+  
+  window.scrollTo({
+    top: elementPosition - offset,
+    behavior: 'smooth'
+  });
+}
+
+createStars(300);
